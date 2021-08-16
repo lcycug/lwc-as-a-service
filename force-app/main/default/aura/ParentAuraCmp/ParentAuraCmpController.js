ba167852592496437a2709d8969c0f5157dacb94
Sean Lee <lcycug@live.com>
2021-08-16 21:45:25 +0800

    Update ParentAuraCmpController.js

--- force-app/main/default/aura/ParentAuraCmp/ParentAuraCmpController.js
+++ force-app/main/default/aura/ParentAuraCmp/ParentAuraCmpController.js
@@ -7,7 +7,7 @@
     checkAccountEligibility: function (component) {
         let idAccount = component.get("v.recordId"),
             service = component.find("service"),
-            { checkEligibilityStatus } = service;
+            { checkEligibilityStatus } = service || {};
         if (checkEligibilityStatus) {
             checkEligibilityStatus(idAccount).then((response) => {
                 component.set("v.boolEligible", response === true);
